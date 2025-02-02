cab8a15266d0604bdad4bc5eba7e92f3cc535302
Mohamed Nabil <ahmedmn@state.gov>
2025-02-02 15:09:33 +0200

    Mediatr Details

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -10,23 +10,16 @@ namespace API.Controllers
 {
     public class ActivitiesController: BaseController
     {
-        private readonly IMediator _mediator;
-
-        public ActivitiesController(IMediator mediator)
-        {
-            _mediator = mediator;
-        }
-
         [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query()); 
+            return await Mediator.Send(new List.Query()); 
         }
 
         [HttpGet("{id}")] //api/activities/sdsd4454sd
         public async Task<ActionResult<Activity>> GetActivity (Guid id)
         {
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
         }
     }
 }
